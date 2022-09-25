f1259417c5501ab3ae4991c49d1ed7975f4fd104
Miguel Perez <a01252536@itesm.mx>
2022-09-25 02:35:11 -0500

    Fixed map height

--- DJ_test/hello/views.py
+++ DJ_test/hello/views.py
@@ -1,4 +1,5 @@
 import json
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.timezone import datetime
@@ -13,6 +14,53 @@ def home(request):
         'hello/hello_there.html',
         {
             'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY,
-            'someDjangoVariable' : json.dumps([{'lat': 25.6487278, 'lng': -100.2899258 }])
+            'someDjangoVariable' : json.dumps([{'lat': 25.6787278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.687278+random.randrange(1,10)*.01, 'lng': -100.289258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.617278+random.randrange(1,10)*.01, 'lng': -100.2859258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6487278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6527278+random.randrange(1,10)*.01, 'lng': -100.2868258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.651278+random.randrange(1,10)*.01, 'lng': -100.279258+random.randrange(1,10)*.01 },
+            {'lat': 25.6787278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.687278+random.randrange(1,10)*.01, 'lng': -100.289258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.617278+random.randrange(1,10)*.01, 'lng': -100.2859258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6487278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6527278+random.randrange(1,10)*.01, 'lng': -100.2868258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.651278+random.randrange(1,10)*.01, 'lng': -100.279258+random.randrange(1,10)*.01 },
+            {'lat': 25.6787278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.687278+random.randrange(1,10)*.01, 'lng': -100.289258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.617278+random.randrange(1,10)*.01, 'lng': -100.2859258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6487278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6527278+random.randrange(1,10)*.01, 'lng': -100.2868258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.651278+random.randrange(1,10)*.01, 'lng': -100.279258+random.randrange(1,10)*.01 },
+            {'lat': 25.6787278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.687278+random.randrange(1,10)*.01, 'lng': -100.289258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.617278+random.randrange(1,10)*.01, 'lng': -100.2859258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6487278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6527278+random.randrange(1,10)*.01, 'lng': -100.2868258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.651278+random.randrange(1,10)*.01, 'lng': -100.279258+random.randrange(1,10)*.01 },
+            {'lat': 25.6787278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.687278+random.randrange(1,10)*.01, 'lng': -100.289258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.617278+random.randrange(1,10)*.01, 'lng': -100.2859258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6487278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6527278+random.randrange(1,10)*.01, 'lng': -100.2868258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.651278+random.randrange(1,10)*.01, 'lng': -100.279258+random.randrange(1,10)*.01 },
+            {'lat': 25.6787278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.687278+random.randrange(1,10)*.01, 'lng': -100.289258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.617278+random.randrange(1,10)*.01, 'lng': -100.2859258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6487278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6527278+random.randrange(1,10)*.01, 'lng': -100.2868258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.651278+random.randrange(1,10)*.01, 'lng': -100.279258+random.randrange(1,10)*.01 },
+            {'lat': 25.6787278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.687278+random.randrange(1,10)*.01, 'lng': -100.289258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.617278+random.randrange(1,10)*.01, 'lng': -100.2859258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6487278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6527278+random.randrange(1,10)*.01, 'lng': -100.2868258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.651278+random.randrange(1,10)*.01, 'lng': -100.279258+random.randrange(1,10)*.01 },
+            {'lat': 25.6787278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.687278+random.randrange(1,10)*.01, 'lng': -100.289258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.617278+random.randrange(1,10)*.01, 'lng': -100.2859258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6487278+random.randrange(1,10)*.01, 'lng': -100.2899258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.6527278+random.randrange(1,10)*.01, 'lng': -100.2868258+random.randrange(1,10)*.01 }, 
+            {'lat': 25.651278+random.randrange(1,10)*.01, 'lng': -100.279258+random.randrange(1,10)*.01 }])
         }
     )
